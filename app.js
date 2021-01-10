3914eace9f59527c8500f00f0c9b54fab9c34182
Alexa Carrington <acarring@wellesley.edu>
2021-01-10 12:14:07 -0500

    Added checkOffItem function in js

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 window.addEventListener('DOMContentLoaded', function() {
-
-    const deleteBtn = document.querySelectorAll('.container .items .items-list .delete');
-    const checkbox = document.querySelectorAll('.container .items .items-list input');
     const addBtn = document.querySelector('.container .add-item .add');
     const itemsList = document.querySelector('.container .items .items-list');
     
@@ -20,9 +17,35 @@ window.addEventListener('DOMContentLoaded', function() {
         `;
         itemsList.append(newListItem);
         addInputElement.value = "";
+
+        const deleteBtn = document.querySelectorAll('.container .items .items-list .delete');
+        deleteBtn.forEach(btn => btn.addEventListener('click', deleteItem));
+
+        const checkboxes = document.querySelectorAll('.container .items .items-list input');
+        checkboxes.forEach(checkbox => checkbox.addEventListener('click', checkOffItem));
     }
     
-    addBtn.addEventListener('click', addItemToList);
+    function deleteItem(e) {
+        let selectedItem = e.target;
+        selectedItem.parentElement.remove();
+    }
 
+    function checkOffItem(e) {
+        let checkedBox = e.target;
+        let checkedItem = e.target.parentElement;
+        checkedBox.classList.toggle('checked');
+
+        if (checkedBox.classList.contains('checked')) {
+            checkedItem.style.textDecoration = "line-through";
+            checkedBox.style.checked = "checked";
+        } else {
+            checkedItem.style.textDecoration = "none";
+            checkedBox.style.checked = "false";
+        }
+
+        
+    }
+
+    addBtn.addEventListener('click', addItemToList);
 
 });
