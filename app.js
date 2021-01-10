fb8663e4b4a1c9b5d574d2c00fcdafbedee1b9a1
Alexa Carrington <acarring@wellesley.edu>
2021-01-10 11:50:57 -0500

    Edited styling, wrapped js in DOMContentLoaded event listener

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const deleteBtn = document.querySelectorAll('.container .items .items-list .delete');
-const checkbox = document.querySelectorAll('.container .items .items-list input');
-const addBtn = document.querySelector('.container .add-item .add');
-const itemsList = document.querySelector('.container .items .items-list');
+window.addEventListener('DOMContentLoaded', function() {
 
-function addItemToList() {
-    let addInputElement = document.querySelector('.container .add-item input');
-    let addInputText = addInputElement.value;
-    let newListItem = document.createElement('div');
-    newListItem.classList.add('list-item');
-    newListItem.innerHTML = `
-                <input type="checkbox">
-                <li class="item">${addInputText}</li>
-                <button class="delete">Delete Item</button>
-    `;
-    itemsList.append(newListItem);
-    addInputElement.value = "";
-}
+    const deleteBtn = document.querySelectorAll('.container .items .items-list .delete');
+    const checkbox = document.querySelectorAll('.container .items .items-list input');
+    const addBtn = document.querySelector('.container .add-item .add');
+    const itemsList = document.querySelector('.container .items .items-list');
+    
+    function addItemToList() {
+        let addInputElement = document.querySelector('.container .add-item input');
+        let addInputText = addInputElement.value;
+        let newListItem = document.createElement('div');
+    
+        if (addInputText === "") return;
+    
+        newListItem.classList.add('list-item');
+        newListItem.innerHTML = `
+                    <input type="checkbox">
+                    <li class="item">${addInputText}</li>
+                    <button class="delete">Delete Item</button>
+        `;
+        itemsList.append(newListItem);
+        addInputElement.value = "";
+    }
+    
+    addBtn.addEventListener('click', addItemToList);
 
-addBtn.addEventListener('click', addItemToList);
+
+});
